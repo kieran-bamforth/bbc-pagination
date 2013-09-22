98cecf13770e143b659e31e82054fedaf2108a3b
Kieran Bamforth <hello@kieranbamforth.me>
2013-09-22 12:14:43 +0100

    - Added common style elements _Common.scss - Added pagination device style elements _NewsTicker.scss - Upated NewsTickerTemplate

--- web/js/templates/NewsTickerTemplate.js
+++ web/js/templates/NewsTickerTemplate.js
@@ -8,21 +8,21 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
 function program1(depth0,data) {
   
   var buffer = "", stack1, options;
-  buffer += "\r\n		<li>";
+  buffer += "\r\n			<li class=\"link\">";
   options = {hash:{},data:data};
   buffer += escapeExpression(((stack1 = helpers.indexNumber || depth0.indexNumber),stack1 ? stack1.call(depth0, ((stack1 = data),stack1 == null || stack1 === false ? stack1 : stack1.index), options) : helperMissing.call(depth0, "indexNumber", ((stack1 = data),stack1 == null || stack1 === false ? stack1 : stack1.index), options)))
-    + "</li>\r\n	";
+    + "</li>\r\n		";
   return buffer;
   }
 
-  buffer += "<ul class=\"news-ticker-pagination-device\">\r\n	<li class=\"previous\"><span class=\"previous-icon\"></span>"
+  buffer += "<section class=\"news-ticker-pagination-device\">\r\n	<ul class=\"inline-list\">\r\n		<li class=\"previous\"><span class=\"previous-icon\"></span>"
     + escapeExpression(((stack1 = ((stack1 = depth0.sender),stack1 == null || stack1 === false ? stack1 : stack1.previousLinkText)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "</li>\r\n	";
+    + "</li>\r\n		";
   stack2 = helpers.each.call(depth0, ((stack1 = depth0.sender),stack1 == null || stack1 === false ? stack1 : stack1.items), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
   if(stack2 || stack2 === 0) { buffer += stack2; }
-  buffer += "\r\n	<li class=\"next\"><span class=\"next-icon\"></span>"
+  buffer += "\r\n		<li class=\"next\"><span class=\"next-icon\"></span>"
     + escapeExpression(((stack1 = ((stack1 = depth0.sender),stack1 == null || stack1 === false ? stack1 : stack1.nextLinkText)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "</li>\r\n</ul>";
+    + "</li>\r\n	</ul>\r\n</section>";
   return buffer;
   });
 })();
