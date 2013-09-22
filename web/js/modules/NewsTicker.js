92062ad3bd4fb70b3057355689df09a42d420ec4
Kieran Bamforth <hello@kieranbamforth.me>
2013-09-22 21:03:11 +0100

    - NewsTicker.js started to add the paginate() function - Added another news article into feed-news.json to add more pages and   thus get a better chance at testing the paginate() function

--- web/js/modules/NewsTicker.js
+++ web/js/modules/NewsTicker.js
@@ -7,17 +7,17 @@ define(['newstickertemplate', 'handlebars'],
 	{
 		newsTickerID++;
 		return {
-			maxLinkCount: 0,
+			maxLinkCount: 11,
 			previousLinkText: "previous",
 			nextLinkText: "next",
 			previousLinkIcon: "prev-icon",
 			nextLinkIcon: "next-icon",
 			previousLinkPosition: 0,
 			nextLinkPosition: 1,
-			truncationCharacter: "...",
-			truncationLinkCountInner: 2,
-			truncationLinkCountOuter: 2,
-			truncationMinimimHiddenLinkCount: 2,
+			truncationCharacter: "...", // The character used to hide items in pagination. 
+			truncationLinkCountInner: 2, // The amount of links to show on either side of the active item.
+			truncationLinkCountOuter: 2, // The amount of links to always show on the beginning and end of the paginated list.
+			truncationMinimimHiddenLinkCount: 2, // The amount of hidden links required to show the truncation character.
 			linksPosition: 1,
 
 			id: newsTickerID,
@@ -47,6 +47,7 @@ define(['newstickertemplate', 'handlebars'],
 				// Assign behaviour when buttons in paginated devices are clicked
 				var newsTicker = this;
 				this.links = this.DOMElement.getElementsByClassName('link');
+				
 				for(var link = 0; link < this.links.length; link++)
 				{
 					this.links[link].addEventListener('click', function(clickEvent)
@@ -63,7 +64,7 @@ define(['newstickertemplate', 'handlebars'],
 						{
 							// Set current story.
 							newsTicker.currentItemIndex = nextItemIndex;
-							// Set CSS classes for prev / next then links.
+							// Set styles for prev / next buttons and then set the links styles.
 							var prevLink = newsTicker.DOMElement.getElementsByClassName('previous')[0],
 								newLink = newsTicker.DOMElement.getElementsByClassName('next')[0];
 							prevLink.className = "previous"+((nextItemIndex == 0)? " disabled" : "");
@@ -71,8 +72,45 @@ define(['newstickertemplate', 'handlebars'],
 							for(var link = 0; link < newsTicker.links.length; link++)
 								 newsTicker.links[link].className = "link";
 							this.className = "link selected";
+							// All styled up, final step: paginate!
+							newsTicker.paginate();
 						}
 					});
+				}
+			},
+			paginate: function()
+			{
+				/* For pagination to be successful there needs to be enough links to vouch for
+					the need of pagination, i.e there should be enough items to require at least 
+					one set of ellipses - therefore we need to create a boundary. */
+				var currentItemIndex = parseInt(this.currentItemIndex)+1;
+				var boundary = this.truncationLinkCountOuter + this.truncationMinimimHiddenLinkCount + this.truncationLinkCountInner + 1;
+				var minimumAmountOfLinksRequiredForPagination = boundary + this.truncationLinkCountInner + this.truncationLinkCountOuter;
+				if(this.links.length >= minimumAmountOfLinksRequiredForPagination)
+				{
+					var output = Array(this.links[0], this.links[1]); // The indexes of the items that will be visible. -1 means elipsis.
+					
+					/* Okay we made it here so begin paginating. If the current link is inside the lower
+						 or upper boundary, we only need to generate one set of ellipsis. */
+					if(currentItemIndex < boundary)
+					{
+						for(var i=this.truncationLinkCountOuter; i<boundary; i++)
+							output.push(this.links[i]);
+						output.push(-1);
+					}
+					else if(currentItemIndex > (this.links.length+1-boundary))
+					{
+						output.push(-1);
+						// The links
+					}
+					// If we make it here, we need 2 sets of ellipses. 
+					else
+					{
+						output.push(-1);
+						// The links
+						output.push(-1);
+					}
+					output.push(this.links[this.links.length-2], this.links[this.links.length-1]);
 				}
 			},
 			/* Occurs when a paginated button is clicked.
